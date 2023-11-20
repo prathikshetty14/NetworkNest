0766895c6da060d7b783c6241e1e56410e379f22
prathikshetty14 <prathikshetty1411@gmail.com>
2023-11-20 22:05:04 +0530

    commit-3

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
